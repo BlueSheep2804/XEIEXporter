b54deac78faf0b442f7e60328a531a4b314161d3
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-02-25 17:18:14 +0900

    feat: 🎸 作業台レシピの材料の配置を改善

--- src/main/java/dev/bluesheep/xeiexporter/exporter/recipe/CraftingRecipeExporter.kt
+++ src/main/java/dev/bluesheep/xeiexporter/exporter/recipe/CraftingRecipeExporter.kt
@@ -5,8 +5,12 @@ import dev.bluesheep.xeiexporter.api.recipe.RecipeData
 import dev.bluesheep.xeiexporter.api.recipe.ingredient.ItemRecipeIngredient
 import dev.bluesheep.xeiexporter.api.recipe.result.ItemRecipeResult
 import dev.bluesheep.xeiexporter.exporter.ExportUtil.rlVanilla
+import net.minecraft.core.NonNullList
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.crafting.CraftingRecipe
+import net.minecraft.world.item.crafting.Ingredient
+import net.minecraft.world.item.crafting.ShapedRecipe
+import net.minecraft.world.item.crafting.ShapelessRecipe
 import net.minecraft.world.level.Level
 
 class CraftingRecipeExporter() : IRecipeExporter<CraftingRecipe> {
@@ -18,11 +22,50 @@ class CraftingRecipeExporter() : IRecipeExporter<CraftingRecipe> {
         recipe: CraftingRecipe,
         level: Level
     ): RecipeData {
-        val ingredients = recipe.ingredients.map { ItemRecipeIngredient(it) }
+        val ingredients = NonNullList.withSize(9, Ingredient.EMPTY)
+        when (recipe) {
+            is ShapedRecipe -> {
+                when (recipe.width) {
+                    1 -> {
+                        recipe.ingredients.forEachIndexed { index, ingredient ->
+                            ingredients[1 + index * 3] = ingredient
+                        }
+                    }
+                    2 -> {
+                        recipe.ingredients.forEachIndexed { index, ingredient ->
+                            ingredients[(index % 2) + (index / 2 * 3)] = ingredient
+                        }
+                    }
+                    3 -> {
+                        recipe.ingredients.forEachIndexed { index, ingredient ->
+                            ingredients[index] = ingredient
+                        }
+                    }
+                }
+            }
+            is ShapelessRecipe -> {
+                when (recipe.ingredients.size) {
+                    1 -> {
+                        ingredients[4] = recipe.ingredients.first()
+                    }
+                    in 2..4 -> {
+                        recipe.ingredients.forEachIndexed { index, ingredient ->
+                            ingredients[(index % 2) + (index / 2 * 3)] = ingredient
+                        }
+                    }
+                    else -> {
+                        recipe.ingredients.forEachIndexed { index, ingredient ->
+                            ingredients[index] = ingredient
+                        }
+                    }
+                }
+            }
+        }
+
         return RecipeData(
             recipe.id,
             recipeTypeId,
-            ingredients,
+            ingredients.map { ItemRecipeIngredient(it) },
             listOf(ItemRecipeResult(recipe.getResultItem(level.registryAccess())))
         )
     }
