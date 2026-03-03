05b261b310053acc422c5cdfe18b5e9c75938141
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-03-04 00:34:59 +0900

    refactor: 💡 ItemExporterの簡略化

--- src/main/java/dev/bluesheep/xeiexporter/exporter/ItemExporter.kt
+++ src/main/java/dev/bluesheep/xeiexporter/exporter/ItemExporter.kt
@@ -1,10 +1,7 @@
 package dev.bluesheep.xeiexporter.exporter
 
-import dev.bluesheep.xeiexporter.exporter.resources.ItemRendererExporter
 import dev.bluesheep.xeiexporter.sql.DatabaseUtil
 import dev.bluesheep.xeiexporter.sql.ItemsTable
-import net.minecraft.resources.ResourceKey
-import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraftforge.registries.ForgeRegistries
 import org.jetbrains.exposed.v1.jdbc.batchInsert
@@ -13,14 +10,11 @@ import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 object ItemExporter {
     fun exportItems() {
         val itemsEntries = ForgeRegistries.ITEMS.getEntries()
-        exportItem(itemsEntries)
-    }
 
-    private fun exportItem(entries: Set<Map.Entry<ResourceKey<Item>, Item>>) {
         transaction {
             DatabaseUtil.reset(ItemsTable)
 
-            ItemsTable.batchInsert(entries) { itemEntry ->
+            ItemsTable.batchInsert(itemsEntries) { itemEntry ->
                 val itemId = itemEntry.key.location()
                 val itemStack = ItemStack(itemEntry.value)
                 this[ItemsTable.namespace] = itemId.namespace
