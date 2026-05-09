a8bfcdd432d43b56729309f3f17cdef7b92f8626
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-05-09 23:42:25 +0900

    feat: 🎸 バニラ連携用のイベントの優先度を最高に設定

--- src/main/java/dev/bluesheep/xeiexporter/compat/minecraft/EventHandler.kt
+++ src/main/java/dev/bluesheep/xeiexporter/compat/minecraft/EventHandler.kt
@@ -5,6 +5,7 @@ import dev.bluesheep.xeiexporter.api.event.RegisterIngredientExtraHelperEvent
 import dev.bluesheep.xeiexporter.api.event.RegisterTagEvent
 import mezz.jei.api.constants.VanillaTypes
 import mezz.jei.api.forge.ForgeTypes
+import net.minecraftforge.eventbus.api.EventPriority
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus
@@ -12,13 +13,13 @@ import net.minecraftforge.registries.ForgeRegistries
 
 @EventBusSubscriber(modid = XEIExporter.MODID, bus = Bus.MOD)
 object EventHandler {
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     fun registerIngredientExtraHelper(event: RegisterIngredientExtraHelperEvent) {
         event.register(VanillaTypes.ITEM_STACK, ItemIngredientExtraHelper())
         event.register(ForgeTypes.FLUID_STACK, FluidIngredientExtraHelper())
     }
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     fun registerTag(event: RegisterTagEvent) {
         event.register(VanillaTypes.ITEM_STACK, ForgeRegistries.ITEMS)
         event.register(ForgeTypes.FLUID_STACK, ForgeRegistries.FLUIDS)
