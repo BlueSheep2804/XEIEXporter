082a7dbf965334f7bfb662363ffd5240a496e0c3
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-05-19 19:50:49 +0900

    refactor: 💡 未使用のテーブル定義を削除

--- src/main/java/dev/bluesheep/xeiexporter/sql/FluidTagsTable.kt
+++
@@ -1,9 +0,0 @@
-package dev.bluesheep.xeiexporter.sql
-
-import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
-
-object FluidTagsTable : IntIdTable("tags_fluid") {
-    val namespace = text("namespace")
-    val path = text("path")
-    val entry = array<String>("entry")
-}
